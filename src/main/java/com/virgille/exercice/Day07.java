933241705fb0cae09eea73a8428ee565ee21df5c
Virgill_e <virgile.bigare@gmail.com>
2025-10-14 08:58:42 +0200

    init day7

---
+++ src/main/java/com/virgille/exercice/Day07.java
@@ -0,0 +1,18 @@
+package com.virgille.exercice;
+
+import com.virgille.Solution;
+
+import java.util.List;
+
+public class Day07 implements Solution {
+    @Override
+    public String part1(List<String> input) {
+        return "";
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        return "";
+    }
+}
+
