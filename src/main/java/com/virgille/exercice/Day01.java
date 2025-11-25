633a91c1c3ec3586d3234b723f4b6837609ef350
Virgill_e <virgile.bigare@gmail.com>
2025-11-25 11:12:34 +0100

    SOON ADVENT OF CODE 2025

--- src/main/java/com/virgille/exercice/Day01.java
+++ src/main/java/com/virgille/exercice/Day01.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Day01 implements Solution {
     @Override
     public String part1(List<String> input) {
-        return "";
+        return "SOON ADVENT OF CODE 2025";
     }
 
     @Override
